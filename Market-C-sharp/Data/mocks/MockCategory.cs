ae10cd24c00876339be46bd0686aaaf70737c774
Dmitriy Balamozhnov <dmitriy.balamozhnov@gmail.com>
2021-12-01 22:04:59 +0300

    Delete MockCategory.cs

--- Market-C-sharp/Data/mocks/MockCategory.cs
+++
@@ -1,5 +0,0 @@
-﻿using Market_C_sharp.Data.Interfaces;using Market_C_sharp.Data.Models;using System;using System.Collections.Generic;using System.Linq;using System.Threading.Tasks;namespace Market_C_sharp.Data.mocks{    public class MockCategory : ICarsCategory    {        public IEnumerable<Category> AllCategories {            get {                return new List<Category> {
-
-                    new Category { categoryName = "Электромобили", description = "Современный вид транспорта" },
-                    new Category { categoryName = "Классические автомобили", description = "машины с двигателем внутреннего сгорания" }
-                };            }        }    }}
