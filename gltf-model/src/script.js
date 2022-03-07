8a175d3b08a0663ff9fb0812c5446d1812f5a361
te1m0z <dmitriy.balamozhnov@gmail.com>
2022-03-07 16:22:55 +0300

    book animation

--- gltf-model/src/script.js
+++ gltf-model/src/script.js
@@ -3,12 +3,16 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import * as dat from 'dat.gui';
 
+const div = document.getElementById('preview');
+
+
 const renderer = new THREE.WebGLRenderer();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100);
+const camera = new THREE.PerspectiveCamera(40, div.clientWidth / div.clientHeight, 1, 100);
 const scene = new THREE.Scene();
-const controls = new OrbitControls(camera, renderer.domElement);
+// const controls = new OrbitControls(camera, renderer.domElement);
 const clock = new THREE.Clock();
 const loader = new GLTFLoader();
+
 
 let Mesh;
 let light;
@@ -16,38 +20,43 @@ let mixer = new THREE.AnimationMixer(Mesh);
 let dt;
 
 function init() {
-    scene.background = new THREE.Color('green');
-    // camera.position.set(0, 0, 10);
+    scene.background = new THREE.Color('grey');
+    camera.position.set(0, 0, 4);
 
-    controls.minPolarAngle = 0.8;
-    controls.maxPolarAngle = 2.5;
-    controls.maxDistance = 3.5;
-    controls.minDistance = 1.8;
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
+    // controls.minPolarAngle = 0.8;
+    // controls.maxPolarAngle = 2.5;
+    // controls.maxDistance = 3.5;
+    // controls.minDistance = 1.8;
+    renderer.setSize(div.clientWidth, div.clientHeight);
+    div.appendChild(renderer.domElement);
 }
 
 function setLight() {
-    light = new THREE.AmbientLight(0xFFFFFF, 2); // soft white light
+    light = new THREE.AmbientLight(0xFFFFFF, 1); // soft white light
     scene.add(light);
 }
 
 function loadGLTF() {
-    loader.load('./catapulta.gltf', (model) => {
+    loader.load('./book.gltf', (model) => {
         Mesh = model.scene;
-        Mesh.position.y = -0.5;
-        mixer = new THREE.AnimationMixer(Mesh);
-        mixer.clipAction(model.animations[1]).play();
+        Mesh.rotation.y = -1;
+        // mixer = new THREE.AnimationMixer(Mesh);
+        // mixer.clipAction(model.animations[1]).play();
         scene.add(Mesh);
     }, animate);
 }
 
+var amplitude = 10;
+var period = 200;
+let curRat;
+let dir = 'left';
+
 function animate() {
-    // if (Mesh && Mesh.rotation) {
-    //     Mesh.rotation.y -= 0.005;
-    // }
-    mixer.update(clock.getDelta());
-    controls.update();
+    if (Mesh && Mesh.rotation) {
+        Mesh.rotation.y -= 0.0007;
+    }
+    // mixer.update(clock.getDelta());
+    // controls.update();
     renderer.render(scene, camera);
     requestAnimationFrame(animate);
 }
@@ -74,4 +83,4 @@ function addGui() {
 init();
 setLight();
 loadGLTF();
-addGui();
+// addGui();
